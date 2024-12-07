db36dbef5a5feab61721c6e0ae52c1504fa8eee0
David Rivera <izaya_sv@outlook.com>
2024-12-07 10:28:29 -0600

    vaciado
    Reset de todas las historias

--- bunko/bunko/settings.py
+++ bunko/bunko/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-135kdya^1r=q^i2t*qmrz^a9xv=&a3_&9-687g_%n4ol#wo!n0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.localhost','izayasan.pythonanywhere.com', '127.0.0.1','192.168.0.104']
+ALLOWED_HOSTS = ['.localhost','svtimes.pythonanywhere.com', '127.0.0.1','192.168.0.104']
 
 
 # Application definition
